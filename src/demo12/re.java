8477ee26de545a67b965fb26abb09922c3c7f6bf
bestrookie <491051587@qq.com>
2020-05-12 18:19:14 +0800

    软件构造课程更新修改

--- src/demo12/re.java
+++ src/demo12/re.java
@@ -3,14 +3,14 @@ package demo12;
 public class re {
 
     public static void main(String[] args) {
-        String a = "asdfdsf";
+        String a = "1asdf123dsf";
         Boolean b;
         boolean dem = demo(a);
         System.out.println(dem);
 
     }
      static boolean demo(String a){
-         String re = "^[A-Za-z]+$";
+         String re = "^[a-zA-Z][A-Za-z0-9]+$";
         boolean jude = a.matches(re);
         return jude;
     }
